b10be2bec7cd01db5d3de9955d92d4f8f3a2e4d0
Robin Hagmayer <robin.hagmayer@proton.me>
2024-09-17 23:19:17 +0200

    Added better keymaps for "^" and "$"

--- init.lua
+++ init.lua
@@ -190,8 +190,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Personal keybinds
+-- [[ Personal Keymaps ]]
 vim.keymap.set('i', 'jk', '<Esc>')
+-- Remap '^' and '$' to better keys in normal, visual, select and operator-pending mode
+vim.keymap.set('', 'H', '^', { desc = 'End of line' })
+vim.keymap.set('', 'L', '$', { desc = 'Start of line (none ws)' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
